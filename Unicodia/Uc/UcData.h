39954a8553bd121be67e604db467c825b508a053
Mercury13 <mercury13@ukr.net>
2021-07-18 01:23:01 +0300

    UcDefines расформирован

--- Unicodia/Uc/UcData.h
+++ Unicodia/Uc/UcData.h
@@ -11,7 +11,6 @@
 #include "u_TypedFlags.h"
 
 // Unicode data
-#include "UcDefines.h"
 #include "UcAutoDefines.h"
 
 /// ALL NAMES HERE ARE IN UTF-8!
@@ -45,6 +44,95 @@ private:
 constexpr Percent operator "" _pc (unsigned long long x) { return Percent(x); }
 
 namespace uc {
+
+    constexpr unsigned NO_CHAR = std::numeric_limits<unsigned>::max();
+
+    enum class EcLangLife
+    {
+        NOMATTER,       ///< Symbols (language’s life does not matter)
+        ALIVE,          ///< UNESCO safe (Ukrainian)
+                        ///< UNESCO vulnerable (Belorussian)
+        ENDANGERED,     ///< UNESCO endangered (Walloon)
+                        ///< UNESCO highly endangered (Crimean Tatar)
+        DEAD,           ///< UNESCO elders only (Votic)
+                        ///< UNESCO dead (Yugh)
+        HISTORICAL,     ///< Dead long ago (Latin, Coptic)
+        NEW,            ///< Newly-created (Adlam)
+        REVIVED,        ///< Revived (Ahom)
+        //ARTIFICIAL,     ///< Artificial languages (Esperanto); unencoded right now
+        //FICTIONAL       ///< Fictional languages (Quenya, Klingon); unencoded right now
+        NN
+    };
+
+    enum class EcScriptType
+    {
+        NONE,
+        UNKNOWN,
+        ALPHABET,       ///< Pure alphabet script (Latin, Cyrillic)
+        PSEUDOALPHABET, ///< Alphabet scripts that use syllables as units
+        CONSONANT,      ///< Consonant script (Hebrew)
+        SYLLABLE,       ///< Syllable script (Hiragana)
+        ALPHASYLLABLE,  ///< Partly alphabet, partly syllable (Iberian)
+        ABUGIDA_MONOLITH, ///< Syllable script where similar syllables have
+                        ///<   similar symbols (Canadian syllable)
+        ABUGIDA_COMBINING, ///< … + characters are combined from consonant and vowel (Thaana)
+        ABUGIDA_BRAHMI, ///< … + based on Brahmi: K=ka, K*=ki, K’=K (Kannada, Thai)
+        HIEROGLYPH,     ///< Pictures of things (CJK, ancient Egyptian)
+        SYLLABOHEROGLYPH, ///< Syllable + hieroglyph (Linear A/B)
+        //SHORTHAND,      ///< Mostly alphabet, but with some simplifications
+        //                ///<   (English stenography)
+        CODE,           ///< System for encoding other scripts (Braille)
+        SYMBOL,         ///< Miscellaneous symbols
+        GAME,           ///< Tabletop games
+        EMOJI,          ///< Emoji pictures
+        NN,
+    };
+
+    enum class UpCategory
+    {
+        CONTROL,
+        FORMAT,
+        LETTER,
+        MARK,
+        NUMBER,
+        PUNCTUATION,
+        SYMBOL,
+        SEPARATOR
+    };
+
+    enum class EcIndustry
+    {
+        NONE,
+        APPLIANCES,             ///< power, play, volume…
+        ASTROLOGY,
+        ASTRONOMY,
+        BUSINESS,               ///< @, telephone, fax…
+        CHEMISTRY,              ///< incl. alchemy
+        COMPUTING,              ///< incl. \, box-drawing
+        ELECTRIC,
+        MACHINEBUILDING,
+        MATHS,
+        MATHS_MULTILINE,        ///< multiline formulae and so on
+        MEDICINE,               ///< incl. dentistry
+        MUSIC,
+        NAVIGATION_TRAFFIC,     ///< traffic sings, detours…
+        NAVIGATION_PEDESTRIAN,  ///< navigation in airports, tourist maps…
+        POETRY,
+        RELIGION,
+        SPORT,
+        NN
+    };
+
+    enum class EcWritingDir
+    {
+        NOMATTER,
+        LTR,
+        RTL,
+        LTR_CJK,
+        LTR_BU,
+        LTR_COL,
+        NN,
+    };
 
     enum class EcContinent {
         NONE,
