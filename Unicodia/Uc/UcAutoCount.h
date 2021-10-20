1667fdfa27e8d6e1ba4b3573d5e1b6e74b64bb95
Mercury13 <mercury13@ukr.net>
2021-10-21 00:46:01 +0300

    U14: первая работа

--- Unicodia/Uc/UcAutoCount.h
+++ Unicodia/Uc/UcAutoCount.h
@@ -3,7 +3,7 @@
 // Automatically generated, do not edit!
 
 namespace uc {
-constexpr int N_CPS = 143924;
-constexpr int N_BLOCKS = 302;
+constexpr int N_CPS = 144762;
+constexpr int N_BLOCKS = 314;
 constexpr int N_NUMERICS = 170;
 }
