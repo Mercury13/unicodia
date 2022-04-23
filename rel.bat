66a1cd0e0bdccb4241e5f34cb82c9559d546f645
Mercury13 <mercury13@ukr.net>
2022-04-24 02:31:32 +0300

    rel.bat now checks for tools

--- rel.bat
+++ rel.bat
@@ -23,6 +23,26 @@
 
 @path %MINGW%;%PATH%
 
+@echo ===== Checking for tools =====
+
+@if exist %SEVENZIP% goto sz_ok
+@echo BAD: 7-zip not found. Install it into standard directory, or change SEVENZIP variable.
+@goto end
+:sz_ok
+@echo 7-zip OK
+
+@if exist %QTDIR%\bin\qmake.exe goto qt_ok
+@echo BAD: Qmake not found. Install Qt, and set QTDIR variable.
+@goto end
+:qt_ok
+@echo Qt OK
+
+@if exist %MINGW%\g++.exe goto mingw_ok
+@echo BAD: MinGW not found. Install it into standard MSYS directory.
+@goto end
+:mingw_ok
+@echo MinGW OK
+
 @echo ===== Creating directories =====
 @if exist %DEPLOY% del /S /Q %DEPLOY%
 @if exist %DEPLOY% rmdir /S /Q %DEPLOY%
