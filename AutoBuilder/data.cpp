efaa38e9369b28ead6c421f6c935cd43f17c0e1b
Mercury13 <mercury13@ukr.net>
2022-08-05 09:15:53 +0300

    decap notes

--- AutoBuilder/data.cpp
+++ AutoBuilder/data.cpp
@@ -30,7 +30,7 @@ const std::unordered_map<std::string_view, DicEntry> dictionary {
     { "AVESTAN",        Dicf::TRIG_SCRIPT },
     { "BALINESE",       Dicf::TRIG_SCRIPT },
     { "BAMUM",          Dicf::TRIG_SCRIPT },
-    { "BASSA",          Dicf::TRIG_SCRIPT },
+    { "BASSA",          Dicf::TRIG_SCRIPT_IF_FIRST }, // Music: bassa = low
       { "VAH",          Dicf::TRIG_SCRIPT },
     { "BATAK",          Dicf::TRIG_SCRIPT },
     { "BENGALI",        Dicf::TRIG_SCRIPT },
