47de3ac5e2b02f2e8ae083d69dbc30fb7d42cb7d
Mercury13 <mercury13@ukr.net>
2022-09-24 16:14:13 +0300

    Icon for Deva ex A

--- Unicodia/Uc/UcData.cpp
+++ Unicodia/Uc/UcData.cpp
@@ -2015,8 +2015,8 @@ constinit const uc::Block uc::blocks[] {
     // Pau Cin Hau OK
     { 0x11AC0, 0x11AFF, { 0x11AC0, EcContinent::ASIA },
             "Pau Cin Hau", { EcScript::Pauc, '0' }, EcScript::Pauc },
-    /// @todo [U15] Devadagari Ex A
-    { 0x11B00, 0x11B5F, { '?', EcContinent::ASIA, Ifg::MISSING },
+    /// @todo [future] Deva ex A temporarily in Funky
+    { 0x11B00, 0x11B5F, { 0x11B07, EcContinent::ASIA },
             "Devanagari Extended-A",
             { EcScript::Deva, 'A' },
             EcScript::Deva, EcFont::FUNKY, Bfg::HAS_DESCRIPTION },
