e59697e22f3861f340f4cf9371660815495aad12
Mercury13 <mercury13@ukr.net>
2022-10-01 22:15:45 +0300

    Correct transformation matrix

--- Font-Source/GlyphWiki/load-glyphs.py
+++ Font-Source/GlyphWiki/load-glyphs.py
@@ -1,7 +1,15 @@
 import fontforge
+import psMat
 
+# get transformation matrix
+mat1 = psMat.translate(0, -600)
+mat2 = psMat.scale(5)
+mat3 = psMat.translate(0, -200)
+mat = psMat.compose(mat1, mat2)
+mat = psMat.compose(mat, mat3)
+
+# get hieroglyphs
 font=fontforge.activeFont()
-
 file = open('hani-tofu.txt', 'r')
 
 for line0 in file:
@@ -10,3 +18,5 @@ for line0 in file:
     svgName = "AutoRemade/{}.svg".format(line)
     glyph = font.createChar(code)
     glyph.importOutlines(svgName, scale=False)
+    glyph.transform(mat, ("round"))
+    glyph.width = 1000
