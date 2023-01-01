dafd62ea43b5b25d72bc932b5723b7c3eaf04d57
Mercury13 <mercury13@ukr.net>
2023-01-01 23:34:20 +0200

    Lat ex add [svg] 71

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x1DC0;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x1E00;
 
 
 template class LruCache<char32_t, QPixmap>;
