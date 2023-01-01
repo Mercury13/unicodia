311d421cc850bdc382fda2e2c788eba79644b691
Mercury13 <mercury13@ukr.net>
2023-01-02 01:36:28 +0200

    Curr [uk] 752 [svg] 75

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x2070;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x20A0;
 
 
 template class LruCache<char32_t, QPixmap>;
