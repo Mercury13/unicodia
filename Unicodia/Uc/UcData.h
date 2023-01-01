c1c1a8a2884b2fd1380830c3f88dd686f28e7353
Mercury13 <mercury13@ukr.net>
2023-01-01 19:39:16 +0200

    Olck [uk] 732 [svg] 63

--- Unicodia/Uc/UcData.h
+++ Unicodia/Uc/UcData.h
@@ -1039,6 +1039,3 @@ inline const uc::Cp& uc::SynthIcon::cp() const { return *cpsByCode[subj]; }
 
 // LibNode
 inline const uc::Version& uc::LibNode::emojiVersion() const { return versionInfo[static_cast<int>(ecEmojiVersion)]; }
-
-/// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x1C00;
