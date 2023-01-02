8343a22541359839aaff0712f3050939f4f93015
Mercury13 <mercury13@ukr.net>
2023-01-02 15:23:13 +0200

    Math B [svg] 94

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x2900;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x2980;
 
 
 template class LruCache<char32_t, QPixmap>;
