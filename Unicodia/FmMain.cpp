2a34fa12ed2135f24f78d5cdd4e719a775f35fb4
Mercury13 <mercury13@ukr.net>
2023-01-02 21:10:30 +0200

    Tfng [uk] 793 [svg] 101

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x2D00;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x2D30;
 
 
 template class LruCache<char32_t, QPixmap>;
