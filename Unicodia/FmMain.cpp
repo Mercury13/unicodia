8dc202d7b1656bdd77ff5906cfa720e46f23290a
Mercury13 <mercury13@ukr.net>
2023-01-02 15:52:56 +0200

    Misc sym/arr [svg] 96

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x2A00;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x2B00;
 
 
 template class LruCache<char32_t, QPixmap>;
