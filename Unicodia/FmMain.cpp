f813bfe1116ffc8dc6258e9c980c01ae344ca896
Mercury13 <mercury13@ukr.net>
2023-01-02 14:16:04 +0200

    Dingbats [svg] 89

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x2600;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x2700;
 
 
 template class LruCache<char32_t, QPixmap>;
