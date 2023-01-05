ab852d78f50c581f0a7521d4acc62937732f214c
Mercury13 <mercury13@ukr.net>
2023-01-05 07:06:42 +0200

    Bopo [uk] 936, [svg] 111

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x30A0;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x3100;
 
 
 template class LruCache<char32_t, QPixmap>;
