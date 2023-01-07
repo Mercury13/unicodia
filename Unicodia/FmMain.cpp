c1f7157212cacdcafe44f20d81699c9ad1e6d5a8
Mercury13 <mercury13@ukr.net>
2023-01-07 21:26:33 +0200

    Cham MymrA [uk] 1043 [svg] 142

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0xA9E0;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0xAA60;
 
 
 template class LruCache<char32_t, QPixmap>;
