a28a3ba2729741c751723dbef39f9c2c7b4f7299
Mercury13 <mercury13@ukr.net>
2023-01-08 00:16:28 +0200

    EthiExA [svg] 145

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0xAAE0;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0xAB00;
 
 
 template class LruCache<char32_t, QPixmap>;
