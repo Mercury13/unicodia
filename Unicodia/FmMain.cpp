dd947e68da8290e64133b1056156a9bb2278d0e5
Mercury13 <mercury13@ukr.net>
2023-01-08 03:15:51 +0200

    ArabPresA [svg] 153

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0xFB00;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0xFB50;
 
 
 template class LruCache<char32_t, QPixmap>;
