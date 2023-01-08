bc41d51c7ce273a0e731f5eadebe46ad6521a111
Mercury13 <mercury13@ukr.net>
2023-01-08 02:22:35 +0200

    HangSyll [svg] 149

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0xABC0;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0xAC00;
 
 
 template class LruCache<char32_t, QPixmap>;
