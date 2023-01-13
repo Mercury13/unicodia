735cccde5647efdb5189934123fc435b0215ed5a
Mercury13 <mercury13@ukr.net>
2023-01-14 01:10:37 +0200

    Armi [uk] 1131 [svg] 187

--- Unicodia/FmMain.cpp
+++ Unicodia/FmMain.cpp
@@ -53,7 +53,7 @@
 #include "LocList.h"
 
 /// @todo [hidpi] Delete this when done!
-constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x10800;
+constexpr int STARTING_CP_OF_MAX_SVG_BLOCK = 0x10840;
 
 
 template class LruCache<char32_t, QPixmap>;
