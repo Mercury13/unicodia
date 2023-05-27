029fabdc6902776a0445dd22ee487498de130eda
Mercury13 <mercury13@ukr.net>
2023-05-27 16:37:49 +0300

    Shafarik → TTF

--- Unicodia/Uc/UcFonts.cpp
+++ Unicodia/Uc/UcFonts.cpp
@@ -131,7 +131,7 @@ constinit const uc::Font uc::fontInfo[] = {
     { "Sylfaen", Ffg::FALL_TO_NEXT },                                           // Georgian Nuskhuri
       { FNAME_DEJAVU, Ffg::FALL_TO_NEXT },                                      // …1 — FALL to Georgian
     { "NotoSerifGeorgian-Regular.ttf" },                                        // Georgian
-    { "Shafarik-Regular.otf", 115_pc },                                         // Glagolitic
+    { "Shafarik-Regular.ttf", 115_pc },                                         // Glagolitic
     { "NotoSansGrantha-Regular.ttf", Ffg::CELL_SMALLER,
             "padding-top:10%; padding-bottom:12%;"_sty },                       // Grantha
     { "NotoSerifGujarati-Regular.ttf", Ffg::DESC_BIGGER, 110_pc },              // Gujarati
