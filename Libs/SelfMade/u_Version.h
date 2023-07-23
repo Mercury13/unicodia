b71ac135b8fe95fe8d5998e7808e13289ebec223
Mercury13 <mercury13@ukr.net>
2023-07-23 03:10:09 +0300

    doc

--- Libs/SelfMade/u_Version.h
+++ Libs/SelfMade/u_Version.h
@@ -24,7 +24,7 @@ struct Version
 
     /// @warning  Works like snprintf
     ///    1. data is always null-terminated.
-    ///    2. Returns data size as if there were enough buffer.
+    ///    2. Returns data size as if there were enough buffer. Actual buffer size is ret+1
     size_t sprintf(char* data, size_t size) const;
     size_t sprintf(std::span<char> data) const { return sprintf(data.data(), data.size()); }
     size_t sprintf(char8_t* data, size_t size) const
