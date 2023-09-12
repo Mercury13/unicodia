d6f93a65eb6dc1e373dc7f2afa7d82d74876521b
Mercury13 <mercury13@ukr.net>
2023-09-12 21:25:53 +0300

    Data is 15.1 now

--- MiscFiles/RawData/load.bat
+++ MiscFiles/RawData/load.bat
@@ -1,7 +1,7 @@
 rem AFAIK, W10 1803+ has built-in cURL
 curl https://www.unicode.org/Public/15.1.0/ucdxml/ucd.all.flat.zip -L --output ucd.all.flat.zip
 curl https://www.unicode.org/Public/15.1.0/ucd/NamesList.txt -L --output NamesList.txt
-curl https://www.unicode.org/Public/draft/emoji/emoji-test.txt -L --output emoji-test.txt
+curl https://www.unicode.org/Public/emoji/15.1/emoji-test.txt -L --output emoji-test.txt
 curl https://mjn.host.cs.st-andrews.ac.uk/egyptian/unicode/signunicode.xml -L --output signunicode.xml
 curl https://mjn.host.cs.st-andrews.ac.uk/egyptian/unicode/signdescriptioneng.xml -L --output signdescriptioneng.xml
 curl https://html.spec.whatwg.org/entities.json -L --output entities.json
