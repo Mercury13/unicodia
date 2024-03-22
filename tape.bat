0b1a07c1a12edb4433745259f0d91a9ae363e9ba
Mercury13 <mercury13@ukr.net>
2024-03-22 10:32:09 +0200

    upd tape.bat

--- tape.bat
+++ tape.bat
@@ -1,10 +1,10 @@
-rem Settings
+@rem Settings
 @set QTDIR=c:\Qt\6.1.3\mingw81_64
 @set MINGW=c:\msys64\mingw64\bin
 @set SEVENZIP="c:\Program Files\7-zip\7z.exe"
 @set EMOJI=NotoEmoji
 
-rem Rest things
+@rem Rest things
 @set BUILD_TAPE=~Build-Tape
 @set PRONAME_TAPE=TapeMaker\TapeMaker.pro
 
