b6942c01b90271439b2116dde3a91bd181bc0b5f
Mercury13 <mercury13@ukr.net>
2024-04-14 22:15:01 +0300

    comment

--- Unicodia/Uc/UcData.cpp
+++ Unicodia/Uc/UcData.cpp
@@ -867,7 +867,7 @@ constinit const uc::Script uc::scriptInfo[] {
     { "Xpeo", QFontDatabase::Any,
         EcScriptType::ALPHASYLLABLE, EcLangLife::DECIPHERED, EcWritingDir::LTR, EcContinent::ASIA,
         Dating::yapprox(-520), EcFont::OLD_PERSIAN },
-    /// @todo [semi-tofu, P1] Sumerian cuneiform shows badly, Noto = M$ (but Noto has more chars!)
+    // Sumerian cuneiform OK. Partly Segoe, partly Noto (they are the same)
     { "Xsux", QFontDatabase::Any,
         EcScriptType::SYLLABOHIEROGLYPH, EcLangLife::DECIPHERED, EcWritingDir::LTR_CUNEIFORM, EcContinent::ASIA,
         Dating::yapprox(-3200), EcFont::CUNEIFORM },
