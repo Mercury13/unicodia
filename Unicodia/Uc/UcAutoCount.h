59bcc23aca50be33fe8d019fc7bff11877563150
Mercury13 <mercury13@ukr.net>
2024-04-27 22:25:09 +0300

    Manually deleted 0C5C and 0CDC

--- Unicodia/Uc/UcAutoCount.h
+++ Unicodia/Uc/UcAutoCount.h
@@ -3,7 +3,7 @@
 // Automatically generated, do not edit!
 
 namespace uc {
-constexpr int N_CPS = 155065;
+constexpr int N_CPS = 155063;
 constexpr int N_BLOCKS = 332;
 constexpr int N_NUMERICS = 216;
 constexpr int N_EMOJI = 3790;
