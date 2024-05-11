baed4aa3847b1e7af7131385119aa9bac3b25684
Mercury13 <mercury13@ukr.net>
2024-05-11 22:35:30 +0300

    Coptic sender function

--- Font-Source/!Scripts/TransferChars/send.py
+++ Font-Source/!Scripts/TransferChars/send.py
@@ -3,18 +3,40 @@ import os
 
 charsToSend = [ [ 0x2CA8, 0x2CF3 ], [ 0xFE24, 0xFE26], 0x0323 ]
 
-def sendChar(file, font, code):
-    xxx
+# Converts charsToSend to set of Unicode CPs
+def getCps():
+    r = set()
+    for v in charsToSend:
+        if type(v) is list:
+            # add range
+            for code in range(v[0], v[1] + 1):
+                r.add(code)
+        else:
+            # add one
+            r.add(v)
+    return r
 
-# open file?
-file = open("send.txt", "w")
+
+# Sends one character, writes its info to file
+def sendChar(file, glyph, code):
+    iSlot = font.findEncodingSlot(code)
+    if iSlot < 0:
+        raise Exception('Character not found')
+    wid = max(glyph.width, 0)
+    fg = glyph.layers[1]   # foreground layer
+    fname = 'u' + format(code, 'X') + '.svg'
+    print(fname + ' ' + str(code) + ' ' + str(wid), file=file, flush=True)
+    fg.export(fname)
+
+
 fontforge.runInitScripts()
 font = fontforge.activeFont()
-for v in charsToSend:
-    if type(v) == list:
-        # send range
-        for code in range(v[0], v[1] + 1):
-            sendChar(file, font, code)
-    else:
-        # send one
-        sendChar(file, font, w)
+cps = getCps()
+# Finally open file
+file = open('send.txt', 'w')
+for glyph in font.glyphs():
+    code = glyph.unicode
+    if code in cps:
+        sendChar(file, glyph, code)
+# close file
+file.close()
