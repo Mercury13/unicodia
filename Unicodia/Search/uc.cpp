5f51378d26513b45e9f5950b7d638b675704bd6d
Mercury13 <mercury13@ukr.net>
2024-07-19 00:34:53 +0300

    Search once again

--- Unicodia/Search/uc.cpp
+++ Unicodia/Search/uc.cpp
@@ -471,15 +471,23 @@ SafeVector<uc::DecodedEmoji> uc::decodeEmoji(std::u32string_view s)
                 goto jumpOver;
             }
         }
-        // Dead end. Found smth?
-        if (lastKnown.result) {
-            REGISTER_RESULT
-            // I do not want to make true Aho-Corasick here! Just back down.
-            index = lastKnown.iLastPos;
+        if (p != &trieRoot) {
+            // Found smth? (never in root)
+            if (lastKnown.result) {
+                REGISTER_RESULT
+                // I do not want to make true Aho-Corasick here! Just back down.
+                index = lastKnown.iLastPos;
+            }
+            // Anyway move to root
+            lastKnown.result = nullptr;
+            p = &trieRoot;
+            // Try again, root’s children are always present
+            auto itChild = p->children->find(c);
+            if (itChild != p->children->end()) {
+                p = &itChild->second;
+                // 1st is never decodeable
+            }
         }
-        // Anyway move to root
-        p = &trieRoot;
-        lastKnown.result = nullptr;
     jumpOver:;
     }
     if (lastKnown.result) {
