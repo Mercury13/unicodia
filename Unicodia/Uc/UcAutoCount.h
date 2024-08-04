4e14e230326513303759d563267627cf50912569
Mercury13 <mercury13@ukr.net>
2024-08-05 01:02:55 +0300

    −9 emoji

--- Unicodia/Uc/UcAutoCount.h
+++ Unicodia/Uc/UcAutoCount.h
@@ -6,7 +6,7 @@ namespace uc {
 constexpr int N_CPS = 155063;
 constexpr int N_BLOCKS = 332;
 constexpr int N_NUMERICS = 216;
-constexpr int N_EMOJI = 3790;
+constexpr int N_EMOJI = 3781;
 constexpr unsigned LONGEST_LIB = 10;  // in codepoints
 constexpr unsigned N_OLDCOMP_SPANS = 36;
 }
