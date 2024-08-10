0def99d006bbf96bc27b62340e425067d8b2ad44
Mercury13 <mercury13@ukr.net>
2024-08-10 11:54:01 +0300

    upd GW Han

--- Font-Source/GlyphWiki/hint.bat
+++ Font-Source/GlyphWiki/hint.bat
@@ -1,1 +1,1 @@
-d:\Soft\FontEditing\ttfautohint.exe --windows-compatibility --stem-width-mode=sss --symbol UnicodiaHan1.ttf UnicodiaHan.ttf >1 2>2
+d:\Soft\FontEditing\ttfautohint.exe --stem-width-mode=sss --symbol UnicodiaHan1.ttf UnicodiaHan.ttf >1 2>2
