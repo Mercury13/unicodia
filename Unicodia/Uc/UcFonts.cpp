72d17b09f34f2117fdbe85420e17968ddaff7c38
Mercury13 <mercury13@ukr.net>
2024-08-10 12:10:45 +0300

    upd Han :)

--- Unicodia/Uc/UcFonts.cpp
+++ Unicodia/Uc/UcFonts.cpp
@@ -108,7 +108,7 @@ constinit const uc::Font uc::fontInfo[] = {
     { "NotoSansCherokee-Regular.ttf" },                                         // Cherokee
     { "NotoSansChorasmian-Regular.ttf" },                                       // Chorasmian
         // CJK chars are square, and there’s always not enough detail → bigger
-    { "SimSun", Ffg::NOHINT_TINY | Ffg::FALL_TO_NEXT | Ffg::BUG_AVOID, 120_pc },// CJK
+    { "SimSun", Ffg::NOHINT_TINY | Ffg::FALL_TO_NEXT | Ffg::BUG_AVOID, 120_pc }, // CJK
       { "SimSun-ExtB", Ffg::NOHINT_TINY | Ffg::FALL_TO_NEXT | Ffg::BUG_AVOID, 120_pc }, // …1
       { FNAME_BABEL, Ffg::NOHINT_TINY | Ffg::FALL_TO_NEXT, 120_pc },            // …2
       { "Microsoft YaHei", Ffg::FALL_TO_NEXT, 120_pc },                         // …3
