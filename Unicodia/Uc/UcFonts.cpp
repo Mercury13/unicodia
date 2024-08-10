41244392a5a58a344d66dddb5aba9ba34ff37e86
Mercury13 <mercury13@ukr.net>
2024-08-10 12:19:00 +0300

    Removed three unused CJK fonts

--- Unicodia/Uc/UcFonts.cpp
+++ Unicodia/Uc/UcFonts.cpp
@@ -108,14 +108,12 @@ constinit const uc::Font uc::fontInfo[] = {
     { "NotoSansCherokee-Regular.ttf" },                                         // Cherokee
     { "NotoSansChorasmian-Regular.ttf" },                                       // Chorasmian
         // CJK chars are square, and there’s always not enough detail → bigger
+    /// @todo [misrender] BabelStone Han will always have greater priority!
     { "SimSun", Ffg::NOHINT_TINY | Ffg::FALL_TO_NEXT | Ffg::BUG_AVOID, 120_pc }, // CJK
       { "SimSun-ExtB", Ffg::NOHINT_TINY | Ffg::FALL_TO_NEXT | Ffg::BUG_AVOID, 120_pc }, // …1
       { FNAME_BABEL, Ffg::NOHINT_TINY | Ffg::FALL_TO_NEXT, 120_pc },            // …2
       { "Microsoft YaHei", Ffg::FALL_TO_NEXT, 120_pc },                         // …3
       { FNAME_HANA_C, Ffg::FALL_TO_NEXT, 120_pc },                              // …4
-      { "MS Gothic", Ffg::FALL_TO_NEXT, 120_pc },                               // …5 Japanese
-      { "Malgun Gothic", Ffg::FALL_TO_NEXT, 120_pc },                           // …6 Korean
-      { FAM_YU, Ffg::FALL_TO_NEXT, 115_pc },                                    // …7 Japanese
       { FNAME_KOREAN, 110_pc},                                                  // …8 for one W7 tofu FFA0
     { FAM_YU, Ffg::FALL_TO_NEXT, 115_pc },                                      // CJK compat
       { "MS Gothic", Ffg::FALL_TO_NEXT, 120_pc },                               // …1
