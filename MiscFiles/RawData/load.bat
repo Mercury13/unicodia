4df98176e4b5f09271963845b8c1f825e5e8bcfd
Mercury13 <mercury13@ukr.net>
2024-08-30 23:13:25 +0300

    Loaded IDS.txt

--- MiscFiles/RawData/load.bat
+++ MiscFiles/RawData/load.bat
@@ -17,6 +17,7 @@ curl https://mjn.host.cs.st-andrews.ac.uk/egyptian/unicode/signdescriptioneng.xm
 curl https://html.spec.whatwg.org/entities.json -L --output entities.json
 rem Moved and changed structure, also JSON appeared → use it!
 rem curl https://dev.w3.org/html5/html-author/charref --output entities.htm
+curl https://babelstone.co.uk/CJK/IDS.TXT --output IDS.txt
 del Unihan_DictionaryLikeData.txt
 del Unihan_NumericValues.txt
 del Unihan_IRGSources.txt
