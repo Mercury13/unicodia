9ed7ff6f8a37840722d815abeda1e6136d8306b0
Mercury13 <mercury13@ukr.net>
2024-11-18 00:30:19 +0200

    Other enclosed alnum chain

--- Unicodia/Uc/UcFonts.cpp
+++ Unicodia/Uc/UcFonts.cpp
@@ -164,7 +164,7 @@ constinit const uc::Font uc::fontInfo[] = {
       { "NotoSansEgyptianHieroglyphs-Regular.otf", Ffg::CELL_BIGGER },          // …1
     { "NotoSansElbasan-Regular.ttf" },                                          // Elbasan
     { "NotoSansElymaic-Regular.ttf" },                                          // Elymaic    
-    { FNAME_KOREAN, Ffg::FALL_TO_NEXT },                                        // Enclosed alnum
+    { FNAME_NOTOSYM1, Ffg::FALL_TO_NEXT | Ffg::GRAPHIC_SAMPLE, 15_top, 115_pc },// Enclosed alnum
       { FNAME_NOTOSYM2, Ffg::GRAPHIC_SAMPLE, 30_bot, 130_pc },                  // …1
     { "NotoSerifEthiopic-Regular.ttf", Ffg::DESC_BIGGER },                      // Ethiopic
     { "Sylfaen", Ffg::FALL_TO_NEXT },                                           // Georgian Nuskhuri
