48488812b9ff70338987cf225318540bcb1eb20b
Mercury13 <mercury13@ukr.net>
2024-12-15 03:52:54 +0200

    comment

--- Unicodia/MyWiki.h
+++ Unicodia/MyWiki.h
@@ -71,8 +71,8 @@ namespace mywiki {
 
     ///  User interface, general features
     ///  A bit yo-yo’ish (Gui = some class, Walker = main window),
-    ///    but see PopupGui class; nicer architecture will require more code
-    ///    for nothing
+    ///    but see PopupGui class (special generic GUI for pop-up windows);
+    ///    nicer architecture will require more code for nothing
     class Gui    // interface
     {
     public:
