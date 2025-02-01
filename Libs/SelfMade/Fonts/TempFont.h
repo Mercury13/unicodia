f2cd5b0b88b9c74620dafd0aa24970ff726a0d7a
Mercury13 <mercury13@ukr.net>
2025-02-01 20:33:39 +0200

    Tests for strange machines

--- Libs/SelfMade/Fonts/TempFont.h
+++ Libs/SelfMade/Fonts/TempFont.h
@@ -16,7 +16,8 @@ extern std::string tempPrefix;
 constexpr bool isPowerOfTwo(unsigned x) { return ((x & (x - 1)) == 0); }
 
 /// Specific platforms we perform “static-assert” unit tests on
-///   (structure itself is platform-independent)
+///   (structure itself is platform-independent,
+///    though need tweaks to non-binary and non-octet machine)
 enum class TfPlat : unsigned char { X64, UNK };
 #if defined(_WIN64) || defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(__amd64__)
     constexpr TfPlat TF_PLAT = TfPlat::X64;
@@ -35,9 +36,20 @@ public:
     void add(unsigned x);
     bool have(unsigned x) const noexcept;
 private:
-    static constexpr unsigned BYTES_PER_ITEM = sizeof(size_t);
+    using Item = size_t;
+    static constexpr auto ITEM_MAX = std::numeric_limits<Item>::max();
+    static constexpr unsigned BYTES_PER_ITEM = sizeof(Item);
     static constexpr unsigned BITS_PER_ITEM = BYTES_PER_ITEM * 8;
-    static_assert(isPowerOfTwo(BYTES_PER_ITEM));
+    static_assert(std::numeric_limits<Item>::radix == 2, "Non-binary machine?");
+    static_assert(isPowerOfTwo(BYTES_PER_ITEM), "Strange machine");
+
+    static constexpr Item ONE = 1;
+
+    static constexpr auto HIBIT = ONE << (BITS_PER_ITEM - 1);
+    static_assert(HIBIT != 0 && HIBIT < ITEM_MAX);
+    static constexpr auto ALLBITS = HIBIT | (HIBIT - 1);
+    static_assert(ALLBITS == ITEM_MAX, "Tweak for yourself to non-octet machine");
+
     // Unit tests: x64
     static_assert(TF_PLAT_X64 ? (BITS_PER_ITEM == 64) : UNTESTED);
 
@@ -56,14 +68,13 @@ private:
         void set(unsigned n);
         bool have(unsigned n);
     private:
-        size_t items[ITEMS_PER_BLOCK];
+        Item items[ITEMS_PER_BLOCK];
 
         static constexpr unsigned ITEM_HI_SHIFT = std::countr_zero(BITS_PER_ITEM);
         static constexpr unsigned ITEM_LO_MASK = BITS_PER_ITEM - 1;
         // Unit tests: x64
         static_assert(TF_PLAT_X64 ? (ITEM_HI_SHIFT == 6) : UNTESTED);
         static_assert(TF_PLAT_X64 ? (ITEM_LO_MASK == 63) : UNTESTED);
-        static constexpr size_t ONE = 1;
     };
     std::vector<std::unique_ptr<Block>> blocks;
 };
