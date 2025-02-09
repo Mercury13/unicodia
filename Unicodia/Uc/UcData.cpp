49bc6eba8465d53d6e965cecf18babe1da6411bf
Mercury13 <mercury13@ukr.net>
2025-02-09 14:15:59 +0200

    Better cast (we’ll overrun these 32 soon)

--- Unicodia/Uc/UcData.cpp
+++ Unicodia/Uc/UcData.cpp
@@ -1042,7 +1042,7 @@ bool uc::Font::doesSupportChar(char32_t subj) const
     // Quick check by supported plane
     if (auto planes = (flags & Ffg::PLANE_ANY).numeric(); planes != 0) {
         unsigned plane = subj >> 16;
-        if (((static_cast<unsigned>(Ffg::PLANE_0) << plane) & planes) == 0)
+        if (((Flags<Ffg>::toStorage(Ffg::PLANE_0) << plane) & planes) == 0)
             return false;
     }
     // Then load and check using one of methods:
