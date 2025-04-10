23c4ed309862607b6fd6144a33c79a058f43cd9e
Mercury13 <mercury13@ukr.net>
2025-04-10 23:05:17 +0300

    typed Subf

--- Unicodia/MyWiki.cpp
+++ Unicodia/MyWiki.cpp
@@ -892,7 +892,7 @@ namespace {
     // As Qt heavily modifies HTML, that’s for HTML versions (it does not increase inter-line)
     constexpr std::u8string_view SMALL_NBSP_HT = u8"<sub>&nbsp;</sub>"sv;  // HTML view is still funky
 
-    enum class Subf {
+    enum class Subf : unsigned char {
         STANDALONE,     ///< standalone number, key: 1'234
         DENSE,          ///< number in dense text, the key is 1234
         HEX             ///< hex number, 89'ABCD
