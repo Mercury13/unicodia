8ca19053f593db36ba30b51205aa708dca320336
Mercury13 <mercury13@ukr.net>
2025-04-19 14:26:21 +0300

    Set name to Unicodia

--- MiscFiles/WinManifest/build.bat
+++ MiscFiles/WinManifest/build.bat
@@ -1,3 +1,3 @@
 del *.pri
-"c:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\makepri.exe" new /cf priconfig.xml /pr ./Root /of Resources.pri
+"c:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\makepri.exe" new /cf priconfig.xml /pr ./Root /of Resources.pri /in Unicodia
 pause
