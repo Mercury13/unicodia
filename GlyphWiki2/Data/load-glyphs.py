45f99d8a37578d94c63ea5f56c4a5a4917344e7c
Mercury13 <mercury13@ukr.net>
2025-05-02 14:20:26 +0300

    Made something, but still does not work

--- GlyphWiki2/Data/load-glyphs.py
+++ GlyphWiki2/Data/load-glyphs.py
@@ -2,9 +2,11 @@ import fontforge
 import psMat
 import os
 
-TEMPFILENAME = 'UnicodiaHan1.ttf'
+TEMPFILENAME = '~UnicodiaHan1.ttf'
 OUTFILENAME = 'UnicodiaHan.ttf'
 HINTER = 'd:/Soft/FontEditing/ttfautohint.exe'
+NODE = 'd:/Soft/NodeJs/node.exe'
+RUN = '../KageForNodeJs/run.js'
 
 # get transformation matrix
 mat1 = psMat.translate(0, -600)
@@ -20,49 +22,77 @@ nHandGlyphs = sum(1 for _ in font.glyphs())
 # import ideographs
 file = open('hani-tasks.txt', 'r')
 baseDefs = ''
+TEMPSVG = '~tmp.svg'
+NODELINE = '{} {} !{}{} >{}'
 for line0 in file:
     line = line0.strip()
     if (line == ''):
         continue
     cmd = line[0]
-    if (cmd = '='):
+    if (cmd == '#'):
+        # COMMAND # comment
+        continue
+    if (cmd == '='):
+        # COMMAND = assign
         thing = line[1:].strip()
-        if (thing != ''):
-            baseDefs = baseDefs + ' ' + thing        
-    elif (cmd = 'G'):
-        #code = int(line, base=16)
-        #svgName = "AutoRemade/{}.svg".format(line)
-        #glyph = font.createChar(code)
-        #glyph.glyphname = "u" + line.upper()
-        #glyph.importOutlines(svgName, scale=False)
-        #glyph.transform(mat)
-        #glyph.width = 1000
-        baseDefs = baseDefs + ' '
-    else
-        raise 
+        if (thing == ''):
+            raise Exception('Command = (assign) must have some data')
+        baseDefs = baseDefs + ' ' + thing        
+    elif (cmd == 'G'):
+        # COMMAND G go
+        thing = line[1:].strip()
+        params = thing.split()
+        if len(params) != 2:
+            raise Exception('Command G (go) must have 2 params')
+        sUcode = params[0]
+        ucode = int(sUcode, base=16)
+        entryPoint = params[1]
+        
+        cmdline = NODELINE.format(NODE, RUN, entryPoint, baseDefs, TEMPSVG)
+        debugFile = open('~hani.log', 'w')
+        debugFile.write(cmdline)
+        debugFile.write('\n')
+        debugFile.close()
+
+        os.system(cmdline)
+        
+        fileSize = os.path.getsize(TEMPSVG)
+        if (fileSize < 200):
+            raise Exception('Probably made an empty file');
+        
+        glyph = font.createChar(ucode)
+        glyph.glyphname = "u" + sUcode.upper()
+        glyph.importOutlines(TEMPSVG, scale=False, simplify=False)
+        glyph.transform(mat)
+        glyph.width = 1000        
+
+        # Stop those base defs
+        baseDefs = ''
+    else:
+        raise Exception('Unknown command ' + cmd)
 
 # Work glyph-by glyph
 # (Somehow it’s quicker and works better)
-index = 0
-for glyph in font.glyphs():
-    if (index >= nHandGlyphs):    
-        # Round and add extrema
-        fg = glyph.layers[1]
-        fg.round()
-        fg.addExtrema("all")
-        fg.round()
-        # Simplify to get rid of poor extrema
-        fg.simplify(0.1)
-        fg.round()
-        # Hint
-        glyph.foreground = fg
-        # Correct direction
-        if not glyph.selfIntersects():
-            glyph.correctDirection()
-    ++index
+# index = 0
+# for glyph in font.glyphs():
+   # if (index >= nHandGlyphs):    
+       # # Round and add extrema
+       # fg = glyph.layers[1]
+       # fg.round()
+       # fg.addExtrema("all")
+       # fg.round()
+       # # Simplify to get rid of poor extrema
+       # fg.simplify(0.1)
+       # fg.round()
+       # # Hint
+       # glyph.foreground = fg
+       # # Correct direction
+       # if not glyph.selfIntersects():
+           # glyph.correctDirection()
+   # ++index
 
-font.generate(TEMPFILENAME)
+#font.generate(TEMPFILENAME)
 
 # Run external hinter
-CMDLINE = '{} --stem-width-mode=sss --symbol {} {}'
-os.system(CMDLINE.format(HINTER, TEMPFILENAME, OUTFILENAME))
+#CMDLINE = '{} --stem-width-mode=sss --symbol {} {}'
+#os.system(CMDLINE.format(HINTER, TEMPFILENAME, OUTFILENAME))
