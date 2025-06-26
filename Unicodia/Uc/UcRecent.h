a81ccb5641a33bf6b75e7c76bc529d0c45fa84e0
Mercury13 <mercury13@ukr.net>
2025-06-26 23:37:57 +0300

    cmt

--- Unicodia/Uc/UcRecent.h
+++ Unicodia/Uc/UcRecent.h
@@ -5,7 +5,7 @@
 //
 //  If the situation changes in 2060:
 //  1. Change all L10n’s you know.
-//  2. Are any L10n’s left
+//  2. Are any L10n’s left?
 //     2.1. No, all → delete here and from MyWiki
 //     2.2. Yes, left → set to 2059
 //
