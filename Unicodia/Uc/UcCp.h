dd3e41ef3bedd1711e0a7549a768e552186812a4
Mercury13 <mercury13@ukr.net>
2025-07-23 10:48:40 +0300

    upd UcCp

--- Unicodia/Uc/UcCp.h
+++ Unicodia/Uc/UcCp.h
@@ -90,18 +90,19 @@ namespace cp {
     constexpr char32_t MALE = 0x2642;
     constexpr char32_t FEMALE = 0x2640;
     constexpr char32_t SANTA_CLAUS = 0x1F385;
-    constexpr char32_t MAN  = 0x1F468;
-    constexpr char32_t WOMAN = 0x1F469;
+    constexpr char32_t BLACK_FLAG = 0x1F3F4;
     constexpr char32_t BOY = 0x1F466;
     constexpr char32_t GIRL = 0x1F467;
-    constexpr char32_t ADULT = 0x1F9D1;
-    constexpr char32_t CHILD = 0x1F9D2;
+    constexpr char32_t MAN  = 0x1F468;
+    constexpr char32_t WOMAN = 0x1F469;
     constexpr char32_t MAN_AND_WOMAN = 0x1F46B;
     constexpr char32_t TWO_MEN = 0x1F46C;
     constexpr char32_t TWO_WOMEN = 0x1F46D;
     constexpr char32_t KISS_MARK = 0x1F48B;
+    constexpr char32_t HANDSHAKE = 0x1F91D;
     constexpr char32_t MRS_CLAUS = 0x1F936;
-    constexpr char32_t BLACK_FLAG = 0x1F3F4;
+    constexpr char32_t ADULT = 0x1F9D1;
+    constexpr char32_t CHILD = 0x1F9D2;
 
     // Smiley
     constexpr char32_t SMILE_SLIGHTLY_SMILING = 0x1F642;
