81cbb62cc046096eae1c90def850253e01aa9351
Mercury13 <mercury13@ukr.net>
2025-11-01 00:19:19 +0200

    Rewrote loader

--- MiscFiles/RawData/load.bat
+++ MiscFiles/RawData/load.bat
@@ -1,5 +1,6 @@
 rem AFAIK, W10 1803+ has built-in cURL
-set UCD=https://www.unicode.org/Public/17.0.0/ucd
+set VER=https://www.unicode.org/Public/17.0.0/
+set UCD=%VER%/ucd
 curl %UCD%/DerivedAge.txt -L --output DerivedAge.txt
 curl %UCD%/NameAliases.txt -L --output NameAliases.txt
 curl %UCD%/NamesList.txt -L --output NamesList.txt
@@ -13,7 +14,7 @@ curl %UCD%/extracted/DerivedName.txt -L --output DerivedName.txt
 curl %UCD%/UnicodeData.txt -L --output UnicodeData.txt
 curl %UCD%/Unikemet.txt -L --output Unikemet.txt
 curl %UCD%/Unihan.zip -L --output Unihan.zip
-curl https://www.unicode.org/Public/draft/emoji/emoji-test.txt -L --output emoji-test.txt
+curl %VER%/emoji/emoji-test.txt -L --output emoji-test.txt
 curl https://mjn.host.cs.st-andrews.ac.uk/egyptian/unicode/signunicode.xml -L --output signunicode.xml
 curl https://mjn.host.cs.st-andrews.ac.uk/egyptian/unicode/signdescriptioneng.xml -L --output signdescriptioneng.xml
 curl https://html.spec.whatwg.org/entities.json -L --output entities.json
