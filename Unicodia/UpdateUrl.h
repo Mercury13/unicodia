50d7cc759106908c82ab78810d0903991456c462
Mercury13 <Mercury13@users.noreply.github.com>
2026-05-31 13:54:53 +0300

    Releases is no longer a constant

--- Unicodia/UpdateUrl.h
+++ Unicodia/UpdateUrl.h
@@ -3,12 +3,12 @@
 namespace upd {
 
 #define N_CHECKED_VERSIONS "8"
-#define SUBURL_REPO "Mercury13/unicodia/releases"
+#define REPO "Mercury13/unicodia"
 
-    constexpr const char* URL_API = "https://api.github.com/repos/" SUBURL_REPO "?per_page=" N_CHECKED_VERSIONS;
-    constexpr const char* URL_REPO = "https://github.com/" SUBURL_REPO;
+    constexpr const char* URL_API = "https://api.github.com/repos/" REPO "/releases?per_page=" N_CHECKED_VERSIONS;
+    constexpr const char* URL_REPO = "https://github.com/" REPO "/releases";
 
 #undef N_CHECKED_VERSIONS
-#undef SUBURL_REPO
+#undef REPO
 
 }
