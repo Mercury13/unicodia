7478714a89787de2c8caee3d5282494eb4a2e219
Mercury13 <Mercury13@users.noreply.github.com>
2026-06-11 00:03:43 +0300

    All basic props

--- SvgOpt/Lib/Xbit.h
+++ SvgOpt/Lib/Xbit.h
@@ -12,6 +12,12 @@ namespace xs {
         CURRENT_COLOR = 1 << 14, ///< Related to currentColor
         INTERACTION = 1 << 15,  ///< Related to interaction
         IMAGE = 1 << 16,        ///< Related to images
+        MULTIMODAL = 1 << 17,   ///< Related to multimodal objects (fill/stroke/markers)
+        SHAPE = 1 << 18,        ///< Shapes
+        SHAPE_STROKE = 1 << 19, ///< Stroke + shape (for markers)
+        OPEN_STROKE = 1 << 20,  ///< Open strokes (for butt)
+        SHARP_STROKE = STROKE,  ///< Strokes with sharp corners
+        JUST_LEAVE = 1 << 25,   ///< Just leave
         RENDERING_HINT = 1<< 26, ///< Rendering hint
         NOT_INHERITED = 1<< 27, ///< Is not inherited
         ATTR_ONLY = 1 << 28,    ///< Only attribute, forbidden in style
@@ -19,10 +25,15 @@ namespace xs {
         FREE_STRING = 1 << 30,  ///< The type is free unparsed string
         COMBO_META = META | ATTR_ONLY | FREE_STRING,
         COMBO_DRAW = FILL | STROKE | TEXT,
+        COMBO_VECTOR = SHAPE | TEXT,
         COMBO_ID_CLIP_PATH = COMBO_DRAW | NOT_INHERITED,
         COMBO_ALL = COMBO_DRAW | GRADIENT | STOP,
         COMBO_ID_CURSOR = COMBO_ALL | INTERACTION,
         COMBO_ID_IMAGE_RENDER = IMAGE | RENDERING_HINT,
+        COMBO_ID_OPACITY = COMBO_DRAW | NOT_INHERITED,
+        COMBO_ID_POINTER_EVENTS = COMBO_DRAW | INTERACTION,
+        COMBO_ID_SHAPE_RENDER = FILL | STROKE | RENDERING_HINT,
+        COMBO_ID_TEXT_RENDER = TEXT | RENDERING_HINT,
     };
 
 }   // namespace xs
--- SvgOpt/Lib/Xid.h
+++ SvgOpt/Lib/Xid.h
@@ -15,7 +15,7 @@ XACT( CURSOR,    "cursor",    xs::IdBit::COMBO_ID_CURSOR )
 XACT( DESC,    "desc",    xs::IdBit::COMBO_META )
 XACT( DISPLAY, "display", xs::IdBit::COMBO_DRAW )
 // F
-XACT( FILL,         "fill",         xs::IdBit::FILL )
+XACT( FILL,         "fill",         xs::IdBit::COMBO_VECTOR )
 XACT( FILL_OPACITY, "fill-opacity", xs::IdBit::FILL )
 XACT( FILL_RULE,    "fill-rule",    xs::IdBit::FILL )
 XACT( FILTER,       "filter",       xs::IdBit::COMBO_DRAW )
@@ -27,14 +27,47 @@ XACT( FONT_STYLE,   "font-style",   xs::IdBit::TEXT )
 XACT( FONT_VARIANT, "font-variant", xs::IdBit::TEXT )
 XACT( FONT_WEIGHT,  "font-weight",  xs::IdBit::TEXT )
 // I
-XACT( IMAGE_RENDERING, "image-rendering", xs::IdBit::COMBO_ID_IMAGE_RENDER )
+XACT( IMAGE_RENDER, "image-rendering", xs::IdBit::COMBO_ID_IMAGE_RENDER )
+// L
+XACT( LETTER_SPACING, "letter-spacing", xs::IdBit::TEXT )
 // M
+XACT( MARKER_END,   "marker-end",   xs::IdBit::SHAPE_STROKE )
+XACT( MARKER_MID,   "marker-mid",   xs::IdBit::SHAPE_STROKE )
+XACT( MARKER_START, "marker-start", xs::IdBit::SHAPE_STROKE )
+XACT( MASK,         "mask",         xs::IdBit::JUST_LEAVE )
 XACT( METADATA,     "metadata",     xs::IdBit::COMBO_META )
+// O
+XACT( OPACITY,  "opacity",  xs::IdBit::COMBO_ID_OPACITY )
+XACT( OVERFLO,  "overflow", xs::IdBit::JUST_LEAVE )  // OVERFLOW is some macro
+// P
+XACT( PAINT_ORDER,    "paint-order",    xs::IdBit::MULTIMODAL )
+XACT( POINTER_EVENTS, "pointer-events", xs::IdBit::COMBO_ID_POINTER_EVENTS )
 // S
-XACT( STOP_COLOR,   "stop-color",   xs::IdBit::STOP )
-XACT( STOP_OPACITY, "stop-opacity", xs::IdBit::STOP )
-XACT( STYLE,        "style",        xs::IdBit::SPECIAL )
+XACT( SHAPE_RENDER,      "shape-rendering",   xs::IdBit::COMBO_ID_SHAPE_RENDER )
+XACT( STOP_COLOR,        "stop-color",        xs::IdBit::STOP )
+XACT( STOP_OPACITY,      "stop-opacity",      xs::IdBit::STOP )
+XACT( STROKE,            "stroke",            xs::IdBit::COMBO_VECTOR )
+XACT( STROKE_DASHARRAY,  "stroke-dasharray",  xs::IdBit::STROKE )
+XACT( STROKE_DASHOFFSET, "stroke-dashoffset", xs::IdBit::STROKE )
+XACT( STROKE_LINECAP,    "stroke-linecap",    xs::IdBit::OPEN_STROKE )
+XACT( STROKE_LINEJOIN,   "stroke-linejoin",   xs::IdBit::SHARP_STROKE )
+XACT( STROKE_MITERLIMIT, "stroke-miterlimit", xs::IdBit::SHARP_STROKE )
+XACT( STROKE_OPACITY,    "stroke-opacity",    xs::IdBit::STROKE )
+XACT( STROKE_WIDTH,      "stroke-width",      xs::IdBit::STROKE )
+XACT( STYLE,             "style",             xs::IdBit::SPECIAL )
 // T
-XACT( TITLE,        "title",        xs::IdBit::COMBO_META )
+XACT( TEXT_ANCHOR,     "text-anchor",     xs::IdBit::TEXT )
+XACT( TEXT_DECORATION, "text-decoration", xs::IdBit::TEXT )
+XACT( TEXT_OVERFLOW,   "text-overflow",   xs::IdBit::TEXT )
+XACT( TEXT_RENDER,     "text-rendering",  xs::IdBit::COMBO_ID_TEXT_RENDER )
+XACT( TITLE,           "title",           xs::IdBit::COMBO_META )
+// U
+XACT( UNICODE_BIDI, "unicode-bidi", xs::IdBit::SPECIAL )
+// V
+XACT( VECTOR_EFFECT, "vector-effect", xs::IdBit::COMBO_DRAW )
+XACT( VISIBILITY,    "visibility",    xs::IdBit::COMBO_DRAW )
+// W
+XACT( WHITE_SPACE,  "white-space",  xs::IdBit::TEXT )
+XACT( WRITING_MODE, "writing-mode", xs::IdBit::TEXT )
 
 #undef XACT
