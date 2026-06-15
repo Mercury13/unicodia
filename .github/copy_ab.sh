0ab29a1fc63a831d18a144fce0336550e2cb6669
Mercury13 <Mercury13@users.noreply.github.com>
2026-06-15 22:13:29 +0300

    Copy AB's artifacts

---
+++ .github/copy_ab.sh
@@ -0,0 +1,6 @@
+cp -f UcAuto.cpp %1/Unicodia/Uc
+cp -f UcAutoLib.cpp %1/Unicodia/Uc
+cp -f UcAutoCount.h %1/Unicodia/Uc
+cp -f UcAutoScripts.h %1/Unicodia/Uc
+cp -f UcAutoOldComp.cpp %1/Unicodia/Uc
+cp -f UcAutoSutton.cpp %1/Unicodia/Uc
--- xgh_copy1.bat
+++
@@ -1,6 +0,0 @@
-copy /y UcAuto.cpp %1/Unicodia/Uc
-copy /y UcAutoLib.cpp %1/Unicodia/Uc
-copy /y UcAutoCount.h %1/Unicodia/Uc
-copy /y UcAutoScripts.h %1/Unicodia/Uc
-copy /y UcAutoOldComp.cpp %1/Unicodia/Uc
-copy /y UcAutoSutton.cpp %1/Unicodia/Uc
