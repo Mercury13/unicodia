9b3e1283b609240758772ec2ac154ebff3ffc760
Mercury13 <Mercury13@users.noreply.github.com>
2026-06-15 22:39:14 +0300

    Partly works, run once again!

--- .github/copy_ab.sh
+++ .github/copy_ab.sh
@@ -1,6 +1,6 @@
-cp -f -v UcAuto.cpp %1/Unicodia/Uc
-cp -f -v UcAutoLib.cpp %1/Unicodia/Uc
-Copy-Item "UcAutoCount.h" "%1/Unicodia/Uc" -Force
-Copy-Item "UcAutoScripts.h" "%1/Unicodia/Uc" -Force
-Copy-Item "UcAutoOldComp.cpp" "%1/Unicodia/Uc" -Force
-Copy-Item "UcAutoSutton.cpp" "%1/Unicodia/Uc" -Force
+cp -f -v UcAuto.cpp $1/Unicodia/Uc
+cp -f -v UcAutoLib.cpp $1/Unicodia/Uc
+cp -f -v UcAutoCount.h $1/Unicodia/Uc
+cp -f -v UcAutoScripts.h %1/Unicodia/Uc
+cp -f -v UcAutoOldComp.cpp %1/Unicodia/Uc
+cp -f -v UcAutoSutton.cpp %1/Unicodia/Uc
