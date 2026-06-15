f729a9b3abb6a54ee402e97e0410453582224eb5
Mercury13 <Mercury13@users.noreply.github.com>
2026-06-15 21:25:52 +0300

    Created a batch file for copying AB's files

---
+++ xgh_copy1.bat
@@ -0,0 +1,6 @@
+copy /y UcAuto.cpp %1/Unicodia/Uc
+copy /y UcAutoLib.cpp %1/Unicodia/Uc
+copy /y UcAutoCount.h %1/Unicodia/Uc
+copy /y UcAutoScripts.h %1/Unicodia/Uc
+copy /y UcAutoOldComp.cpp %1/Unicodia/Uc
+copy /y UcAutoSutton.cpp %1/Unicodia/Uc
