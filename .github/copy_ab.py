00ff4cd02f6fbf4e9dfd857bcead2d5db67dd170
Mercury13 <Mercury13@users.noreply.github.com>
2026-06-16 14:17:19 +0300

    SmartCopy through Python?

---
+++ .github/copy_ab.py
@@ -0,0 +1,35 @@
+# Python version of SmartCopy, with a small kink:
+# it doen not replace versioned file, instead just
+# checks equality.
+
+import sys
+import shutil
+import os
+
+print('Python SmartCopy!')
+
+params = sys.argv[1:]
+if len(params) < 1:
+    print('Usage: copy_ab.py dest_dir')
+    sys.exit(1)
+    
+filesToCopy = [ 'UcAuto.py' ]
+
+filesToCheck = [ 'UcAutoLib.cpp', 'UcAutoCount.h',
+    'UcAutoScripts.h', 'UcAutoOldComp.cpp', 'UcAutoSutton.cpp' ]
+
+for x in filesToCopy:
+    destPath = os.path.join(sys.argv[1], x)
+    shutil.copy(x, destPath)
+    print('Copied {x} to {destPath}')
+
+for x in filesToCheck:
+    with open(x, 'r') as file:
+        srcData = file.read()    
+    destPath = os.path.join(sys.argv[1], x)
+    with open(destPath, 'r') as file:
+        destData = file.read()
+    if srcData != destData:
+        print('Files {x} and {destPath} are inequal!')
+        sys.exit(1)
+    print('Files {x} and {destPath} are equal')
