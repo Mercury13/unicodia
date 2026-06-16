44a5d4486b6efeb4ddd66f31521dfca83f338fc5
Mercury13 <Mercury13@users.noreply.github.com>
2026-06-16 22:04:06 +0300

    Attempt to deploy Unicodia using Python

--- .github/copy_ab.sh
+++
@@ -1,6 +0,0 @@
-cp -f -v UcAuto.cpp $1/Unicodia/Uc
-cp -f -v UcAutoLib.cpp $1/Unicodia/Uc
-cp -f -v UcAutoCount.h $1/Unicodia/Uc
-cp -f -v UcAutoScripts.h $1/Unicodia/Uc
-cp -f -v UcAutoOldComp.cpp $1/Unicodia/Uc
-cp -f -v UcAutoSutton.cpp $1/Unicodia/Uc
---
+++ .github/deploy_win.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import shutil
+
+print('Deploy Unicodia on Windows')
+
+params = sys.argv[1:]
+if len(params) < 1:
+    print('Usage: deploy_win.py qtdir')
+    exit(1)
+
+QT_DIR = sys.argv[1]
+print(f'The Qt dir is {QT_DIR}.')
+
+FILES = [
+    'build-UC|Unicodia.exe',
+    QT_DIR + '/bin|Qt6Core.dll',
+]
+DIR_DEPLOY = 'deploy-UC'
+os.mkdir(DIR_DEPLOY)
+
+for v in FILES:
+    v = v.replace('//', '/')
+    v = v.replace('/|', '|')
+    whereBar = v.find('|')
+    if whereBar < 0:
+        print(f'Cannot find a bar in "{1}"')
+        sys.exit(1)
+    subDirAndFile = v[(whereBar + 1):]
+    # @todo [urgent] no paths
+    paths = whereBar.split('/')
+    if len(paths > 0):
+        print(f'Additional subpaths unsupported!')
+        sys.exit(1)
+    destDir = DIR_DEPLOY
+    srcName = v.replace('|', '/')
+    shutil.copy(srcName, DIR_DEPLOY)
+    print(f'Copied {srcName} --> {DIR_DEPLOY}.')
