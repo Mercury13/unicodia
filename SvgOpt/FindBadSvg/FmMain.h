2825f3ee2f903c5be1627668a497ab35613cac07
Mercury13 <mercury13@ukr.net>
2026-09-19 11:03:35 +0300

    Emoji 1F31E: gradient Xlink

---
+++ SvgOpt/FindBadSvg/FmMain.h
@@ -0,0 +1,23 @@
+#ifndef FMMAIN_H
+#define FMMAIN_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class FmMain;
+}
+QT_END_NAMESPACE
+
+class FmMain : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit FmMain(QWidget *parent = nullptr);
+    ~FmMain() override;
+
+private:
+    Ui::FmMain *ui;
+};
+#endif // FMMAIN_H
