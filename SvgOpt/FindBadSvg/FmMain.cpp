69af435c5a9b122baf087147502e6933ab8d4ef1
Mercury13 <mercury13@ukr.net>
2026-09-19 14:36:40 +0300

    Probably checker works

--- SvgOpt/FindBadSvg/FmMain.cpp
+++ SvgOpt/FindBadSvg/FmMain.cpp
@@ -5,11 +5,117 @@
 
 #include "work.h"
 
+///// ResetLock ////////////////////////////////////////////////////////////////
+
+ResetLock::ResetLock(LockableModel& aModel) : model(aModel)
+{
+    model.beginResetModel();
+}
+
+ResetLock::~ResetLock()
+{
+    model.endResetModel();
+}
+
+
+///// QSep /////////////////////////////////////////////////////////////////////
+
+namespace {
+
+    class QSep
+    {
+    public:
+        QString& target;
+
+        QSep(QString& aTarget, QString aSep)
+            : target(aTarget), sp(std::move(aSep)), len(target.length()) {}
+        void sep();
+        void sepAnd(const QString& s);
+        void sepAnd(const char* s);
+    private:
+        QString sp;
+        qsizetype len;
+    };
+
+    void QSep::sep()
+    {
+        if (target.length() != len) {
+            target += sp;
+        }
+    }
+
+    void QSep::sepAnd(const QString& s)
+    {
+        sep();
+        target += s;
+    }
+
+    void QSep::sepAnd(const char* s)
+    {
+        sep();
+        target += s;
+    }
+
+}   // anon namespace
+
+
+///// BadModel /////////////////////////////////////////////////////////////////
+
+QVariant BadModel::data(const QModelIndex &index, int role) const
+{
+    if (static_cast<size_t>(index.row()) >= files.size())
+        return {};
+    auto& row = files[index.row()];
+    switch (role) {
+    case Qt::DisplayRole: {
+            QString r = QString::fromStdWString(row.name) + ": ";
+            if (row.isValid) {
+                QSep sp(r, ", ");
+                if (row.discrep.hasClipPath) {
+                    sp.sepAnd("clipPath");
+                }
+                if (row.discrep.hasRgbaColor) {
+                    sp.sepAnd("rgba colour");
+                }
+                if (row.discrep.hasGradientXlink) {
+                    sp.sepAnd("gradient Xlink");
+                }
+            } else {
+                r += "UNREADABLE";
+            }
+            return r;
+        }
+    default:
+        return {};
+    }
+}
+
+
+///// GoodModel ////////////////////////////////////////////////////////////////
+
+QVariant GoodModel::data(const QModelIndex &index, int role) const
+{
+    if (static_cast<size_t>(index.row()) >= files.size())
+        return {};
+    auto& row = files[index.row()];
+    switch (role) {
+    case Qt::DisplayRole:
+        return QString::fromStdWString(row);
+    default:
+        return {};
+    }
+}
+
+
+///// FmMain ///////////////////////////////////////////////////////////////////
+
 FmMain::FmMain(QWidget *parent)
     : Super(parent)
     , ui(new Ui::FmMain)
 {
     ui->setupUi(this);
+    ui->listBad->setModel(&badModel);
+    ui->listGood->setModel(&goodModel);
     connect(ui->btBrowse, &QPushButton::clicked, this, &This::browseForDirectory);
     connect(ui->btGo, &QPushButton::clicked, this, &This::go);
 }
@@ -30,6 +136,10 @@ void FmMain::browseForDirectory()
 
 void FmMain::go()
 {
+    ResetLock _1(badModel), _2(goodModel);
+
     std::filesystem::path dir = ui->edDirectory->text().toStdWString();
     auto r = processDir(dir);
+    badModel.take(std::move(r.bad));
+    goodModel.take(std::move(r.good));
 }
--- SvgOpt/FindBadSvg/FmMain.h
+++ SvgOpt/FindBadSvg/FmMain.h
@@ -2,12 +2,55 @@
 #define FMMAIN_H
 
 #include <QMainWindow>
+#include <QAbstractTableModel>
+
+#include "work.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class FmMain;
 }
 QT_END_NAMESPACE
+
+class LockableModel : public QAbstractTableModel
+{
+public:
+    friend class ResetLock;
+protected:
+    using QAbstractTableModel::beginResetModel;
+    using QAbstractTableModel::endResetModel;
+};
+
+class ResetLock
+{
+public:
+    ResetLock(LockableModel& aModel);
+    ~ResetLock();
+private:
+    LockableModel& model;
+};
+
+class BadModel : public LockableModel
+{
+public:
+    int rowCount(const QModelIndex&) const override { return files.size(); }
+    int columnCount(const QModelIndex&) const override { return 1; }
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    void take(std::vector<BadFile>&& x) { files = std::move(x); }
+private:
+    std::vector<BadFile> files;
+};
+
+class GoodModel : public LockableModel
+{
+public:
+    int rowCount(const QModelIndex&) const override { return files.size(); }
+    int columnCount(const QModelIndex&) const override { return 1; }
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    void take(std::vector<std::wstring>&& x) { files = std::move(x); }
+private:
+    std::vector<std::wstring> files;
+};
 
 class FmMain : public QMainWindow
 {
@@ -20,6 +63,8 @@ public:
 
 private:
     Ui::FmMain *ui;
+    BadModel badModel;
+    GoodModel goodModel;
 
 private slots:
     void browseForDirectory();
