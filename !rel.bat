10499ff81bc9d1a4195943f714684f3fe40e8da8
Mercury13 <mercury13@ukr.net>
2026-09-19 22:18:44 +0300

    Double-check in rel

--- !rel.bat
+++ !rel.bat
@@ -147,39 +147,6 @@
 @goto end
 :exe_ok
 
-@echo ===== Checking for tools =====
-
-@if exist %SEVENZIP% goto sz_ok
-@echo BAD: 7-zip not found. Install it into standard directory, or change SEVENZIP variable.
-@goto end
-:sz_ok
-@echo 7-zip OK
-
-@if exist %QTDIR%\bin\qmake.exe goto qt_ok
-@echo BAD: Qmake not found. Install Qt, and set QTDIR variable.
-@goto end
-:qt_ok
-@echo Qt OK
-
-@if exist %MINGW%\g++.exe goto mingw_ok
-@echo BAD: MinGW not found. Install it into standard MSYS directory.
-@goto end
-:mingw_ok
-@echo MinGW OK
-
-@if exist %UTRANSL% goto utransl_ok
-@echo BAD: UTransCon not found.
-@echo Build it manually from https://github.com/Mercury13/utranslator
-@goto end
-:utransl_ok
-@echo UTranslator OK
-
-@if exist %INNO% goto inno_ok
-@echo BAD: Inno Setup not found. It’s a well-known free program.
-@goto end
-:inno_ok
-@echo Inno Setup OK
-
 @echo.
 @echo ===== Copying files =====
 @copy %BUILD%\%EXENAME% %DEPLOY%
